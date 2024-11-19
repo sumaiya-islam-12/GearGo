0e32cd68a8751e80babde6cb117b124af313e0cc
Jannatul711 <jfmim711@gmail.com>
2024-11-19 15:12:00 +0600

    geargo

---
+++ src/Pages/Bike/Bike.jsx
@@ -0,0 +1,146 @@
+
+
+export default function Bike() {
+  return (
+    <div>
+        <section class="banner">
+    <div class="container">
+        <div class="card-area">
+            <div class="card">
+                <h1>Best GPS Tracker For <br> Bikes In Nepal</h1>
+                <p>Ensure Your Bike's Security with Trackon <br> Bike GPS Tracker</p>
+            </div>
+
+        {/* form section */}
+        
+            <div class="card">
+                <form class="form">
+                   <h4>Get an offer in your mobile</h4>
+                    <input type="text" placeholder="Phone Number"/><br>
+                    <input type="text" placeholder="Number of vehicles"/><br>
+                    <button type="submit">Send</button>
+                </form>
+            </div>
+        </div>  
+    </div>
+  </section>
+  {/*  Banner section close */}
+
+
+{/* <!-- feature section start --> */}
+ <div class="container">
+  <div class="features-section">
+    <h2>-Why Is Trackon GPS The Best GPS Bike Tracker In Nepal?-</h2>
+
+      <div class="card-area">
+        <div class="feature">
+            <h4><i class="fa-solid fa-location-dot icon"></i>Real-Time Bike Tracking System</h4>
+            <p>Stay assured about the security of your bike, no matter where it is. Trackon Bike GPS tracker allows you to track and monitor your asset intelligently to avoid theft.
+            </p>
+        </div>
+        <div class="feature">
+            <h4><img src="./Bike-images/Icon material-settings-remote.png" class="icon"/> Bike Lock-Unlock</h4>
+            <p>The bike GPS device from Trackon allows you to lock and unlock your bike remotely. Use our bike GPS tracker to maintain complete control over your vehicle.            </p>
+        </div>
+        <div class="feature">
+            <h4><i class="fa-solid fa-bell icon"></i>Multiple Alerts</h4>
+            <p>Get instant SMS/Notification alerts of specific events such as ignition turn, over-speeding, geo-fence, bike theft attempt, etc., with our bike GPS security system.            </p>
+        </div>
+        <div class="feature">
+            <h4><i class="fa-solid fa-shield-halved icon"></i>Bike Security</h4>
+            <p>Our Bike GPS tracking system gives you complete control over your vehicle. You can keep track of your bike's movements and make sure it's parked where it's intended to be.            </p>
+        </div>
+        <div class="feature">
+            <h4><img src="./Bike-images/Icon ionic-md-speedometer.png" class="icon"/> Fuel Expenses</h4>
+            <p>Trackon can help you save a lot of money on petrol and keep track of your fuel costs. With our GPS tracking app, you can monitor your riding habits and save your expenses daily.            </p>
+        </div>
+        <div class="feature">
+            <h4><img src="./Bike-images/Icon ionic-md-speedometer (1).png" class="icon"/>Monitor Fuel Expenses</h4>
+            <p>Trackon can help you save a lot of money on petrol and keep track of your fuel costs. With our GPS tracking app, you can monitor your riding habits and save your expenses daily.            </p>
+        </div>   
+    </div>
+    <button type="submit">Request Demo</button>
+  </div>
+</div>
+  {/* <!-- feature section close --> */}
+
+{/* <!-- checkbox-secion start --> */}
+  <section class="comparison-section">
+    <h2>-Trackon's Bike GPS Tracking Devices Vs Other Unbranded Devices-</h2>
+      <table>
+        <thead>
+          <tr>
+            <th>Features</th>
+            <th>Trackon</th>
+            <th>Other Devices</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>Real-Time Tracking</td>
+            <td><input type="checkbox" class="custom-checkbox check"/></td>
+            <td><input type="checkbox" class="custom-checkbox"/></td>
+          </tr>
+          <tr>
+            <td>Ignition Alerts</td>
+            <td><input type="checkbox" class="custom-checkbox checked"/></td>
+            <td><input type="checkbox" class="custom-checkbox checked"/></td>
+          </tr>
+          <tr>
+            <td>Vehicle Lock</td>
+            <td><input type="checkbox" class="custom-checkbox checked"/></td>
+            <td><input type="checkbox" class="custom-checkbox checked"/></td>
+          </tr>
+          <tr>
+            <td>1 year Device Warranty</td>
+            <td><input type="checkbox"class="custom-checkbox"/></td>
+            <td><input type="checkbox"class="custom-checkbox"/></td>
+          </tr>
+          <tr>
+            <td>Fuel Monitoring</td>
+            <td><input type="checkbox" class="custom-checkbox"/></td>
+            <td><input type="checkbox" class="custom-checkbox"/></td>
+          </tr>
+          <tr>
+            <td>Over-Speeding Report</td>
+            <td><input type="checkbox" class="custom-checkbox"/></td>
+            <td><input type="checkbox" class="custom-checkbox"/></td>
+          </tr>
+          <tr>
+            <td>Location History ( 90 Days )</td>
+            <td><input type="checkbox" class="custom-checkbox"/></td>
+            <td><input type="checkbox" class="custom-checkbox"/></td>
+          </tr>
+          <tr>
+            <td>Geo-Fencing</td>
+            <td><input type="checkbox" class="custom-checkbox"></td>
+            <td><input type="checkbox" class="custom-checkbox"></td>
+          </tr>   
+        </tbody>
+      </table>
+  </section>
+  <!-- checkbox-section close -->
+
+  <!-- Tracker section start -->
+  <section class="tracker-section">
+    <div class="container">
+        <div class="card-area">
+            <div class="card">
+                <img src="./Bike-images/undraw_traveling_yhxq 1.png" alt="">
+            </div>
+        
+            <div class="card">
+              <h4>Get the Most Affordable Bike GPS Tracker in Nepal</h4>
+              <p>Managing a single motorcycle or a fleet of them can be a difficult task that significantly impacts your budget. And if you go out for the best bike GPS tracker price in Nepal, you might not be able to find one that is affordable and meets your needs. Trackon GPS, on the other hand, offers the most excellent GPS tracker for bike price in Nepal and excellent service. Our bike GPS tracker may serve as a one-stop-shop for all of your bike management requirements. Our wide range of services is made available to you across Nepal, allowing you to enjoy efficient vehicle management without worrying about the cost of a GPS tracking device in Nepal.</p>
+                
+              <button type="submit">Buy Now</button>
+                
+            </div>
+        </div>  
+    </div>
+  </section>
+
+
+    </div>
+  )
+}
