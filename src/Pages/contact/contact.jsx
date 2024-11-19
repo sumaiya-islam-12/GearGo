1d9a7806770bc37041ea8d144ef62d0f4e9d03da
Rabeyakhanom <abdullaabir2026@gmail.com>
2024-11-19 15:10:08 +0600

    Rabeya

---
+++ src/Pages/contact/contact.jsx
@@ -0,0 +1,20 @@
+import { Col, Row } from "react-bootstrap";
+
+
+export default function contact() {
+  return (
+    
+    <div>
+        
+        <h1>-Contact-</h1>
+        <Row>
+        <Col>
+        <img src="/src/assets/Contact/img/undraw_connecting_teams_re_hno7 1.png" alt="" />
+        </Col>
+        <Col>
+        
+        </Col>
+      </Row>
+    </div>
+  )
+}
