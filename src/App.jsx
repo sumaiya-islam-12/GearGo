6048873f649e05cad04c3590fb29918111a9dbcc
Mitu-khanom <mitukhanom009@gmail.com>
2024-11-19 14:36:02 +0600

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import './App.css'
 import Header from './Components/Header/Header'
+import AboutUs from './Pages/About-Us/AboutUs'
 
 function App() {
 
   return (
     <>
     <Header></Header>
+    <AboutUs></AboutUs>
       <h1>Hello</h1>
       <h2>Mitu Khanom</h2>
 
--- src/main.jsx
+++ src/main.jsx
@@ -3,11 +3,16 @@ import { createRoot } from 'react-dom/client';
 import './index.css';
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import Home from './Pages/Home/Home.jsx';
+import AboutUs from './Pages/About-Us/AboutUs.jsx';
 
 const router = createBrowserRouter([
   {
     path: '/',
     element: <Home/>
+  },
+  {
+    path: '/aboutUs',
+    element: <AboutUs/>
   }
 ]);
 
