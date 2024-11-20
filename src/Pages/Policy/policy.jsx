2fdae20ffee2aceaea4c00fa86f17cfca2a41227
mahinkhanom <mahinkhanom58@gmail.com>
2024-11-20 13:58:25 +0600

    mahin added

--- src/Pages/Policy/policy.jsx
+++ src/Pages/Policy/policy.jsx
@@ -3,30 +3,38 @@ import Header from '../../Components/Header/Header';
 
 export default function policy() {
   return (
+    <>
     <div>
       <Header/>
       <div className="Policy-container">
-          <div className="sidebar">
-            <ul>
-              <li>Link to privacy policy...</li>
-              <li>Link To other sites</li>
-              <li>Use of personal information.</li>
-              <li>Log Data</li>
-              <li>Cookies</li>
-              <li>service providers</li>
-              <li>Security</li>
-              <li>Link to other sites</li>
-              <li>WhatsApp policy</li>
-              <li>Children privacy</li>
-              <li>Consent</li>
-              <li>Contact us</li>
-            </ul>
+          <div>
+          <ul>
+                <li>Link to privacy policy...</li>
+                <li>Link To other sites</li>
+                <li>Use of personal information.</li>
+                <li>Log Data</li>
+                <li>Cookies</li>
+                <li>service providers</li>
+                <li>Security</li>
+                <li>Link to other sites</li>
+                <li>WhatsApp policy</li>
+                   <li>Children privacy</li>
+                 <li>Contact us</li>    
+
+
+                     <li>Children privacy</li>
+                 <li>Contact us</li>     
+              
+              </ul>
           </div>
-            <div></div>
+          <div>
+            <h1>Main</h1>
+          </div>
       </div>
 
-
+      
       <Footer></Footer>
     </div>
+    </>
   )
 }
