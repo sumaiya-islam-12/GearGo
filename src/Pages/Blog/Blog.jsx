a770856a6ace0ca973d7e341c86e0ecb3e044a2f
alminnaharkeya <kalminnaharkeya2020@gmail.com>
2024-11-20 16:23:56 +0600

    first commit

--- src/Pages/Blog/Blog.jsx
+++ src/Pages/Blog/Blog.jsx
@@ -1,11 +1,38 @@
+import { useEffect, useState } from 'react';
+import Keya from '../../Components/Blogkeya/keya';
 import Footer from '../../Components/Footer/Footer';
 import Header from '../../Components/Header/Header';
+import './Blog.css';
 
-export default function Blog() {
+
+export default function Blog(){
+  const [blog , setBlog] = useState ([]);
+  useEffect(() => {
+    fetch('/public/Blog.json')
+    .then(res => res.json())
+    .then(data =>setBlog(data))
+  }
+
+  )
+  
+  
   return (
     <div>
         <Header/>
-      <h1>Blog</h1>
+        <div className='container'>
+          <div className='heading'>
+            <p>-Blog-</p>
+          </div>
+          <div className='blog-area'>
+            {
+              blog.map(blogs => <Keya blogdata = {blogs}/>)
+            }
+          </div>
+          
+        <div>
+        
+        </div>
+        </div>
 
       <Footer/>
     </div>
