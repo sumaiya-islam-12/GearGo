ad7830f65eb40f164dc42ae053eba1db1883310a
riktadhali <riktadhali031@gmail.com>
2024-11-23 01:26:29 +0600

    contact

--- src/Pages/Contact/Contact.jsx
+++ src/Pages/Contact/Contact.jsx
@@ -3,13 +3,13 @@ import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import Footer from '../../Components/Footer/Footer';
 import Header from '../../Components/Header/Header';
-
+import './Contact.css'
 
 export default function Contact() {
   return (
     <div>
         <Header/>
-      <h1>-Contact-</h1>
+      <h1 className='contact-tex'>-Contact-</h1>
      <div>
       <Container>
      <Row>
@@ -17,20 +17,20 @@ export default function Contact() {
         <img src="/src/assets/Contact/img/undraw_connecting_teams_re_hno7 1.png" alt="" />
         </Col>
         <Col>
-        <from>
-          <input type='name'placeholder=' Name'>
-          </input>
-          <input type='Phone Number'placeholder='Phone Number'>
-           </input>
-          <input type='Email'placeholder='Email'>
-           </input>
-          <input type='Country/Region'placeholder='Country/Region'>
-           </input>
-          <input type='Interested Product'placeholder='Interested Product'>
-           </input>
-           <input type='Message'placeholder='Message'>
-           </input>
-           <input type='check box'placeholder='check box'></input>
+        <from className="Container-from">
+          <input type='name'placeholder=' Name'/>
+          
+          <input type='Phone Number'placeholder='Phone Number'/>
+        
+          <input type='Email'placeholder='Email'/>
+         
+          <input type='Country/Region'placeholder='Country/Region'/>
+           
+          <input type='Interested Product'placeholder='Interested Product'/>
+           
+           <input type='Message'placeholder='Message'/>
+          
+           <input type='check box'placeholder='check box'/>
 
            
            <button> Send</button>
