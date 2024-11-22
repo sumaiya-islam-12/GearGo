5f63386449b5f03b641cfe9a9928591680e5bf46
riktadhali <riktadhali031@gmail.com>
2024-11-22 20:11:26 +0600

    contact

--- src/Pages/Contact/Contact.jsx
+++ src/Pages/Contact/Contact.jsx
@@ -3,7 +3,7 @@ import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import Footer from '../../Components/Footer/Footer';
 import Header from '../../Components/Header/Header';
-import './Contact.css'
+
 
 export default function Contact() {
   return (
