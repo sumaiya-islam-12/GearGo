bcab7b585e7b8c4602fb465e1a310d1cb5248554
saimajahan27 <ls9894894@gmail.com>
2024-11-25 16:23:04 +0600

    added

--- src/Pages/Download/download.jsx
+++ src/Pages/Download/download.jsx
@@ -1,14 +1,37 @@
-import Footer from '../../Components/Footer/Footer';
-import Header from '../../Components/Header/Header';
+
+import "./Download.css";
+
 
 export default function Download() {
   return (
-    <div>
-        <Header/>
-      <p>Get Gear Go on All your Devices</p>
-      
-
-      <Footer/>
+    <div className="container">
+    {/* First Div */}
+    <div className="text">
+      <h1>-Get Gear Go on All your Devices-</h1>
     </div>
+
+    {/* Second Div */}
+    <div className="image-area">
+      <img src="/src/assets/img/download.png" alt="" />
+    </div>
+
+    {/* Third Div */}
+    <div className="button-area">
+      <button>Download </button>
+      <button>Download </button>
+      <button>Download </button>
+    </div>
+
+    {/* Fourth Div */}
+    <div className="phone-area">
+      <img src="/src/assets/img/download-2.png" alt="" />
+    </div>
+    {/* Fifth Div */}
+    <div className="store-div">
+      <img src="" alt="" />
+      <img src="/src/assets/img/App-store.jpg" alt="" />
+    </div>
+  </div>
   )
 }
+
