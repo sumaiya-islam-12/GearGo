4d3be3caef1df04c046932a0a59d9bc79eb2a7cf
mahinkhanom <mahinkhanom58@gmail.com>
2024-12-01 16:00:09 +0600

    added

--- src/Pages/Policy/policy.jsx
+++ src/Pages/Policy/policy.jsx
@@ -131,7 +131,7 @@ export default function policy() {
           </section>
           <section>
             <div className='in'>
-            Children’s privacy
+            Children privacy
             </div>
             <div className='group-429'>
             These Services do not address anyone under the age of 13. We do not knowingly collect personally identifiable information from children under 13. In the case we discover that a child under 13 has provided us with personal information, we immediately delete this from our servers. If you are a parent or guardian and you are aware that your child has provided us with personal information, please contact us so that we will be able to do necessary actions.
