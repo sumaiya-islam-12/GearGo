600acfc89dd3aeda37b971088c9a554f4d0b7149
Mitu Gazi <MituGazi101@gmail.com>
2024-12-01 16:32:44 +0600

    mitu gazi

--- src/Pages/cart/cart.jsx
+++ src/Pages/cart/cart.jsx
@@ -3,37 +3,72 @@ import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import Footer from '../../Components/Footer/Footer';
 import Header from '../../Components/Header/Header';
+import './Cart.css';
 
 export default function cart() {
   return (
     <div>
          <Header/>
-        <Container>
-            <Row className="justify-content-md-center">
-                <Col xs lg="2">
+         <Container>
+
+         <Row>
+            <Col md={2}>
                 <div>
-                <img src="/src/assets/product-img/Product img (1).png" alt="" />
+                    <div><img className='cart-side-img' src="/src/assets/product-img/Product img (1).png" alt="" /></div>
+                    <div><img className='cart-side-img' src="/src/assets/product-img/Product img (1).png" alt="" /></div>
+                    <div><img className='cart-side-img' src="/src/assets/product-img/Product img (1).png" alt="" /></div>
+                    <div><img className='cart-side-img' src="/src/assets/product-img/Youtube product demo.png" alt="" /></div>
                 </div>
-                </Col>
-                <Col md="auto"><img src="/src/assets/product-img/Product img (1).png" alt=""/>
-                </Col>
-                <Col xs lg="2">
+            </Col>
+            <Col md={6}>
                 <div>
-                    <img src="src/assets/product-img/Product img (1).png" alt="" />
+                <img  className='img-2' src="/src/assets/product-img/Rectangle 102.png" alt="" />
                 </div>
-                </Col>
-            </Row>
-            <Row>
-                <Col></Col>
-                <Col md="auto"><img src="/src/assets/product-img/Youtube product demo.png" alt="" />
-                </Col>
-                <Col xs lg="2">
+            </Col>
+            <Col md={4}>
                 <div>
-                <img src="/src/assets/product-img/Rectangle 102.png" alt="" />
+                    <h1 className='mm'>gearGo Micro with Vi Sim - 1 year</h1>
                 </div>
-                </Col>
-            </Row>
-        </Container>
+                <div className='mind'>
+                    <button className='btn-1'>Scooter</button>
+                    <button className='btn-2'>car</button>
+                    <button className='btn-3'>Bike</button>
+                    <button className='btn-4'>Truck</button>
+                </div>
+                    <div className='text'>
+                        <button className='btn-5'>4.5</button>
+                        <h5>7,000+ Reviews</h5>
+                    </div>
+                    <div className='text2'>
+                    <h5>Extra ₹3690 discount</h5>
+                    </div>
+                    <div className='tu'>
+                    <h4> Deal Price : ₹3190 54% off</h4>
+                    </div>
+                    <div className='mp'>
+                    <h4> Regular Price : ₹6880</h4>
+                    </div>
+                    <div className='a'>
+                    <h4> Choose SIM Operator</h4>
+                    </div>
+                    <div className='b'>
+                    <button className='btn-b'>Airtel</button>
+                    <button className='btn-b'>Vodaphone</button>
+                    </div>
+                    <div className='c'>
+                    <h4> Choose Variant</h4>
+                    </div>
+                    <div>
+                    <button className='btn-d'>Vodaphone</button>
+                    <button className='btn-d'>Vodaphone</button>
+                    <button className='btn-d'>Vodaphone</button>
+                    </div>
+                 
+                 
+                
+            </Col>
+        </Row>
+         </Container>
     <Footer/>
 
 
