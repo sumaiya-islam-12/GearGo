ef236cd3fec58985ae6c6beecde70d8af047ee47
momotadhali <mamotadhali1998@gmail.com>
2024-12-01 16:35:08 +0600

    payment

--- src/Pages/pyment/pyment.jsx
+++ src/Pages/pyment/pyment.jsx
@@ -3,6 +3,8 @@ import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import Footer from '../../Components/Footer/Footer';
 import Header from '../../Components/Header/Header';
+import './pyment.css';
+import Form from 'react-bootstrap/Form';
 
 
 export default function pyment() {
@@ -14,13 +16,14 @@ export default function pyment() {
       </div>
 
       <div className='h4-tex'>
-          <h4>Billing details</h4>
-          <h4>Your order</h4>
+          
+            <div >
               <Container>
-              <Row>
+              <Row className='col'>
                 <Col>
-                    <form>
-                        <div>
+                  <h4 className='row'>Billing details</h4>
+                    <form className='Container-tex'>
+                        <div className='type'>
                             <input type='First name *'placeholder='First name *'/>
                             <input type='last name *'placeholder='last name *'/>
                         </div>
@@ -28,50 +31,103 @@ export default function pyment() {
                             <div className='type-tex'>
                                   <input type='Email *'placeholder='Email *'/>
                                   <input type='Phone *'placeholder='Phone *'/>
-                                  <input type='Country *'placeholder='Country *'/>
-                                  <input type='state *'placeholder='state *'/>
-                                  <input type='city/town *'placeholder='city/town *'/>
-                                  <input type='Zip Code *' placeholder='Zip Code *'/>
-                            </div>
+                             </div>
+                                  
+                                    <div className='type-2'>
+                                        <input type='Country *'placeholder='Country *'/>
+                                        <input type='state *'placeholder='state *'/>
+                                     </div> 
+                                          <div className='type-3' >
+                                            <input type='city/town *'placeholder='city/town *'/>
+                                            <input type='Zip Code *' placeholder='Zip Code *'/>
+                                        </div>
                     </form>
                 </Col>
                
                   <Col>
-                  <form>
-                      <input type='Product'placeholder='Product'/>
-                      <input type='Total'placeholder='Total'/>
-                      <input type='product name'placeholder='product name'/>
-                      <input type='$99.00'placeholder='$99.00'/>
-                      <input type='Subtotal'placeholder='Subtotal'/>
-                      <input type='$99.00'placeholder='$99.00'/>
-                      <input type='Total'placeholder='Total'/>
-                      <input type='$99.00'placeholder='$99.00'/>
-                  <div>
-                        <input type='credit card'placeholder='credit card'/>
-                          <img src='/src/assets/Payment/Img/PngItem_124356 1.png'/>
-                        <input type='Pay pal'placeholder='Pay pal'/>
-                        <img src='/src/assets/Payment/Img/PngItem_124356 1.png'/>
-                  </div>
-                  <div>
-                  <h6>
-                  your personal data will be used only to process your order, support your experience throughout this website,and for other purposes described in our privacy policy
-                  </h6>
-                  <input type='checkbox'/>
-                  <label>
-                  i have read and agree t the website terms and conditions
-                  </label>
-                  </div>
+                  <div className='product-area'>
+                  <h4>Your order</h4> 
+                       <div className='product'> 
+                          <div>
+                            <h5>Product</h5> 
+                          </div>
+                          <div>
+                            <h6>Total </h6>
+                          </div>
+                       </div>
+
+                          <div className='product-name'> 
+                            <div><h5>productName</h5></div>
+                          <div> <h6> $99.00</h6></div>
+                          </div>
+
+                          <div className=' Subtotal'>     
+                          <div><h5>Subtotal</h5></div>
+                          <div>  <h6>$99.00</h6></div>
+                         </div>
+                      
+                          <div className='Total'> 
+                            <div>
+                              <h6>Total</h6>
+                            </div>
+                           <div>
+                           <h6> $99.00</h6>
+                           </div>
+                          </div>
+
+                              <div className='check'>
+
+                              <div  className='Check-type'>
+                              <Form.Check type="radio" aria-label=" " />
+                              <h6>  credit card</h6>
+                              </div>
+                              <div>
+                              <img src='/src/assets/Payment/Img/PngItem_124356 1.png'/>
+                              </div>
+
+                              </div>
+                                  <div className='pay'>
+                                
+                                  <div className='paypal'> 
+                                    <Form.Check type="radio" aria-label=" " />  
+                                    <h6> Pay pal</h6>
+                                  </div>
+                                  <div>
+                                   <img src='/src/assets/Payment/Img/PngItem_124356 1.png'/>
+                                  </div>
+                                   
+                                   </div>
+                                  
+                              
+                              <div>
+                                <h6>
+                                your personal data will be used only to process your order, support your experience throughout this website,and for other purposes described in our privacy policy
+                                </h6>
+
+                               <div className='label'>
+                                  <div className=''>
+                                    <input type='checkbox'/> 
+                                  </div>
+                                  <div>
+                                    <h6>i have read and agree t the website terms and conditions</h6>
+                                  </div>
+                               </div>
+                                
+                              </div>
               
-                  <button>
-                  proceed to pay
-                  </button>
+                              <div className='product-button'>
+                                <button>proceed to pay</button>
+                              </div>
                   
                   
-                  </form>
-                  </Col>
-              </Row>
-            </Container>
-       </div>
+                  </div>
+                
+                     </Col>
+               </Row>
+         </Container>
+      </div>  
+
+    </div>
 
 
 
