58f08316710fc8aa130d6e4949d38473d8ab54a6
Rabeyakhanom <abdullaabir2026@gmail.com>
2024-12-03 14:39:13 +0600

    rabeya-contact

--- src/Pages/Contact/Contact.jsx
+++ src/Pages/Contact/Contact.jsx
@@ -9,45 +9,47 @@ export default function Contact() {
   return (
     <div>
         <Header/>
-     
       <h1 className='contact-tex'>-Contact-</h1>
      <div>
       <Container>
-     <Row>
+     <Row className='row'>
         <Col>
         <img src="/src/assets/Contact/img/undraw_connecting_teams_re_hno7 1.png" alt="" />
         </Col>
         <Col>
         <form className="Container-from">
+          <input type='Name'placeholder=' Name'/>
 
+          <div className='type'>
+                  <input type='Phone Number'placeholder='Phone Number'/>
+                
+                <input type='Email'placeholder='Email'/>
+          </div>
+          
+                
+                  <input type='Country/Region'placeholder='Country/Region'/>
+                  
+                  <input type='Interested Product'placeholder='Interested Product'/>
+           
+                <div className='gap'>
+                      <input type='Message'placeholder='Message'/>
+                </div>
+          
 
-        <input type='Name'placeholder='Name'/>
-
-<input type='Phone Number'placeholder='Phone Number'/>
-
-<input type='Email'placeholder='Email'/>
-
-<input type='Country/Region'placeholder='Country/Region'/>
- 
-<input type='Interested Product'placeholder='Interested Product'/>
- 
- <input type='Message'placeholder='Message'/>
-<div className='check'>
-<input type='checkbox'/>
-  <legend className='text'>
-  By submitting this information, I agree to abide by Jimis privacy and I give my consent for Jimi Laboratories to process and store my personal information.
-  </legend>
-</div>
-
- 
- <button> Send</button>
-
-</form>
-</Col>
-</Row>
-</Container>
-</div>
-<Footer/>
-</div>
-)
+                    <div className='checkbox-Container'>
+                                    <input type='checkbox'/>
+                                    <label>
+                                    By submitting this information, I agree to abide by Jimis privacy and I give my consent for Jimi Laboratories to process and store my personal information.
+                                    </label>
+                   </div>
+           <button> Send</button>
+        
+        </form>
+        </Col>
+      </Row>
+    </Container>
+     </div>
+      <Footer/>
+    </div>
+  )
 }
